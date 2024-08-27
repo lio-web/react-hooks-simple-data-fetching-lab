7e68797bebc34eb021627105acda0c45bab68132
lionard <leonard.muhati@student.moringaschool.com>
2024-08-27 06:28:38 +0300

    compelete

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,39 @@
-// create your App component here
+import React, { useEffect, useState } from "react";
+
+function App() {
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+  const [dogImage, setDogImage] = useState(null);
+
+  useEffect(() => {
+    const fetchDogImage = async () => {
+      try {
+        const response = await fetch("https://dog.ceo/api/breeds/image/random");
+        const data = await response.json();
+        setDogImage(data.message);
+        setLoading(false);
+      } catch (err) {
+        setError("Failed to load data");
+        setLoading(false);
+      }
+    };
+
+    fetchDogImage();
+  }, []);
+
+  if (loading) {
+    return <p>Loading...</p>;
+  }
+
+  if (error) {
+    return <p>{error}</p>;
+  }
+
+  return (
+    <div>
+      <img src={dogImage} alt="A Random Dog" />
+    </div>
+  );
+}
+
+export default App;
